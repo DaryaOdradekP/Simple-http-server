b9b942bc043fff7cb539d25d7261b38f4aa54c87
Darya Pavlova <dasha.pavlova289@gmail.com>
2026-08-25 14:13:00 +0500

    log incoming HTTP request headers

--- SimpleHttpServer.java
+++ SimpleHttpServer.java
@@ -1,16 +1,27 @@
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class SimpleHttpServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             System.out.println("Server started: http://localhost:8080");
 
-            try (Socket clientSocket = serverSocket.accept();
-                 OutputStream output = clientSocket.getOutputStream()) {
+            try (Socket clientSocket = serverSocket.accept(); BufferedReader input = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8)); OutputStream output = clientSocket.getOutputStream()) {
+
+                System.out.println("Request from browser:");
+
+                String line;
+                while ((line = input.readLine()) != null && !line.isEmpty()) {
+                    System.out.println(line);
+                }
 
                 String html = "<h1>Hello from Java!</h1>";
                 byte[] body = html.getBytes(StandardCharsets.UTF_8);
